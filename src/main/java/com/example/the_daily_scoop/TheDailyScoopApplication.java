e3f80638e32a222483cf1f191156c1a92f7b65a7
David Kumar <david.dn.kumar@gmail.com>
2021-02-23 14:54:12 -0600

    init spring app

---
+++ src/main/java/com/example/the_daily_scoop/TheDailyScoopApplication.java
@@ -0,0 +1,13 @@
+package com.example.the_daily_scoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheDailyScoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheDailyScoopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/the_daily_scoop/TheDailyScoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.the_daily_scoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheDailyScoopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
